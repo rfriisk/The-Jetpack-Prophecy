1b0a9a4f3b35677f7b17d39f02a34e5db0298b36
Rasmus Knudsen <64476703+rfriisk@users.noreply.github.com>
2023-07-03 14:32:58 +0200

    Character added

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+     
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * 5f, rb.velocity.y);
+
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
+        }
+    }
+
+
+}
